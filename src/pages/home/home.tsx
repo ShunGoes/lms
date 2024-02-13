0ed05995bf1538f9adc96eee6060c61fe1e2fd65
shunGoes <shunnode@gmail.com>
2024-02-13 12:13:53 +0100

    added a link to dashboard on the hompage

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>
+      This app is under development
+        </h1> 
+      <Link to='dashboard' className="text-blue-500">Visit the dashboard page to see recent development</Link>
+    </div>
   )
 }
 
