abd8d2fb727539daefd0ecf68af773e93b3a5403
shunGoes <shunnode@gmail.com>
2024-02-13 12:21:04 +0100

    working on mobile screen

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
   return (
-    <div>
+    <div className="text-white">
       <h1>
       This app is under development
         </h1> 
