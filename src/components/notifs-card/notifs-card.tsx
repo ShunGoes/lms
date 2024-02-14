4b9430a8d5cf49282e7070b35d4952e5a85ec17c
Sunday Robert <pelurobert10@gmail.com>
2024-02-14 14:47:50 +0100

    notification page and assessment page

--- src/components/notifs-card/notifs-card.tsx
+++ src/components/notifs-card/notifs-card.tsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import { useState } from "react";
+import { Notification } from "../../components/notifs-data/notifs-data";
 
-const NotifsCard = () => {
-  return (
-    <div className='w-full h-[5rem] border-4'>
-
-    </div>
-  )
+interface NotificationCardProps {
+  notification: Notification;
+  onMarkRead: (id: number, read: boolean) => void;
+  onDelete: (id: number) => void;
 }
 
-export default NotifsCard
+const NotifsCard: React.FC<NotificationCardProps> = ({
+  notification,
+  onMarkRead,
+  onDelete,
+}) => {
+  const [checked, setChecked] = useState(notification.read);
+
+  const handleCheckedBox = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.target;
+    setChecked(checked);
+    onMarkRead(notification.id, checked);
+  };
+
+  const handleDelete = () => {
+    onDelete(notification.id);
+  };
+
+  const markAsRead = () => {
+    onMarkRead(notification.id, true);
+    setChecked(true);
+  };
+
+  return (
+    <div className="border-4 flex justify-between p-1 mb-2">
+      <div className="flex gap-1">
+        <div>
+          <input
+            type="checkbox"
+            checked={checked}
+            onChange={handleCheckedBox}
+          />
+        </div>
+        <div>
+          <h3> {notification.type} </h3>
+          <p>{notification.message} </p>
+        </div>
+      </div>
+      <div className="flex gap-2">
+        {!checked && <button onClick={markAsRead}>Mark as read</button>}
+        <button onClick={handleDelete}> Delete </button>
+      </div>
+    </div>
+  );
+};
+
+export default NotifsCard;
--- src/pages/notification-page/notification-page.tsx
+++ src/pages/notification-page/notification-page.tsx
@@ -1,8 +1,51 @@
+import { useState } from "react";
+import NotifsCard from "../../components/notifs-card/notifs-card";
+import { notifsTest } from "../../components/notifs-data/notifs-data";
 
 const NotificationPage = () => {
-  return (
-    <div>NotificationPage</div>
-  )
-}
+  const [notifications, setNotifications] = useState(notifsTest);
+  const [showRead, setShowRead] = useState(true);
 
-export default NotificationPage
+  const handleRead = (id: number, read: boolean) => {
+    const updateNotifs = notifications.map((notification) => {
+      if (notification.id === id) {
+        return { ...notification, read };
+      }
+      return notification;
+    });
+    setNotifications(updateNotifs);
+  };
+
+  const filteredNotifs = showRead
+    ? notifications.filter((notification) => notification.read)
+    : notifications;
+
+  const deleteNotification = (id: number) => {
+    const updateNotifs = notifications.filter(
+      (notification) => notification.id !== id
+    );
+    setNotifications(updateNotifs);
+  };
+
+  return (
+    <div>
+      <h1>NotificationPage</h1>
+      <div className="flex gap-2">
+        <button onClick={() => setShowRead(false)}>All</button>
+        <button onClick={() => setShowRead(true)}>Read</button>
+      </div>
+      <div>
+        {filteredNotifs.map((notification) => (
+          <NotifsCard
+            key={notification.id}
+            notification={notification}
+            onMarkRead={handleRead}
+            onDelete={deleteNotification}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NotificationPage;
